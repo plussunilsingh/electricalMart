52ba027acce0f85bc759b8086a5961c71b99d773
suniltomar <plussunilsingh@gmail.com>
2026-04-04 19:42:56 +0530

    pipleine work4

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,19 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
